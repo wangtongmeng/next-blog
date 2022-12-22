db222fa453dcc30f1c8d902dc7e36851dac2cfd1
wangtongmeng <tongmengwang@163.com>
2022-12-22 21:54:29 +0800

    导航

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,29 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { navs } from './config';
+import styles from './index.module.scss';
 
 const Navbar: NextPage = () => {
-  return <div>导航</div>;
+  const { pathname } = useRouter();
+  return (
+    <div className={styles.navbar}>
+      <section className={styles.logoArea}>Blog</section>
+      <section className={styles.linkArea}>
+        {navs?.map((nav) => {
+          return (
+            <Link
+              className={pathname === nav?.value ? styles.active : ''}
+              key={nav?.label}
+              href={nav?.value}
+            >
+              {nav?.label}
+            </Link>
+          );
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default Navbar;
