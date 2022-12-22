ab4619eb25cfb557cd368b5871ffdb51693381a4
wangtongmeng <tongmengwang@163.com>
2022-12-22 11:22:32 +0800

    工程配置

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  var a = 2;
+
   return (
     <>
       <Head>
@@ -119,5 +121,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
